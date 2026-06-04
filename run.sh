9391af85ec84d9b7f143a4a80352c5280c4fe96a
harukujo227 <stark092004@gmail.com>
2026-06-04 10:07:01 +0200

    add pre-commit hook

--- run.sh
+++ run.sh
@@ -2,12 +2,13 @@
 
 fail() {
 	echo "Not a git repository (or any of the parent directories): .git.
-Do NOT download the repository as a zip file from GitHub.com!
+Do NOT download the repository as a zip file from github.com!
 Please download deadlocked by cloning the Git repository: 'git clone https://github.com/avitran0/deadlocked'"
 	exit 1
 }
 
 [[ -d '.git' ]] || fail
 
+git config core.hooksPath .hooks
 git pull
 cargo run --release
